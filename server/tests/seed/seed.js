6e31076071f21c0f068615ca7991360c3e8006f5
Kevin Baremore <kevinbaremore07@gmail.com>
2017-09-24 20:14:02 -0700

    associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -12,24 +12,30 @@ const users = [{
   _id: userOneId,
   email: 'kevin@example.com',
   password: 'userOnePass',
-  tokens[{
+  tokens: [{
     access: 'auth',
     token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
   }]
 } {
   _id: userTwoId,
   eemail: 'jen@example.completed',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
